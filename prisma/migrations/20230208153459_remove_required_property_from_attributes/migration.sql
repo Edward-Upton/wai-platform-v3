6a9bbe71b1a1377d171880fc118788881bf168de
Edward Upton <ed.upton@outlook.com>
2023-05-17 12:58:26 +0100

    fix schema

--- prisma/migrations/20230208153459_remove_required_property_from_attributes/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `required` on the `Attribute` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Attribute" DROP COLUMN "required";
